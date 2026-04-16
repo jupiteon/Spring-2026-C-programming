90ee34ac9b9bb7dc6debfd743070456bd96fd88c
jupiteon <250471@newuu.uz>
2026-04-16 13:22:05 +0500

    change

--- test.cpp
+++ test.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(){
-    int arr[5] = {10, 20, 30, 40, 50}; 
-    int* p = arr;
-    for (int i = 0; i < 5; ++i) {
-        cout << *(p + i) << " "; 
-    }
-    return 0;
+    int arr[] = {10, 20, 30, 40, 50};
+    int *ptr = arr; // points to arr[0]
+    ptr++; // now points to arr[1]
+    cout << *ptr; // prints 20
+    cout << *(ptr + 2); // prints 40
+    int size = sizeof(arr) / sizeof(int);
 }
