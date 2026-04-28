da6cf605860777e481889ce381ae9b123029e419
jupiteon <250471@newuu.uz>
2026-04-28 10:59:11 +0500

    changes

---
+++ lab13/5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char* w[5];
+    char ch[100];
+    
+    for (int i=0; i<5; i++){
+        cout << "Enter word " << i+1 << ": ";
+        cin >> ch;
+        
+        int len;
+        while (ch[len]!='\0') i++;
+
+        
+    }
+
+    
+
+
+
+
+    return 0;
+}
